88d0207c8b28eb7425d8cca2c6386443046c7cf3
will <will@spanner.org>
2009-03-14 11:25:07 +0000

    site jumper confined to pages pages

--- lib/multi_site/helper_extensions.rb
+++ lib/multi_site/helper_extensions.rb
@@ -3,21 +3,6 @@ module MultiSite::HelperExtensions
 
     base.module_eval do
 
-      def default_page_title
-        title + ' - ' + (current_site.subtitle || '')
-      end
-
-      def title
-        current_site.name
-      end
-
-      def subtitle
-        current_user && admin? ? site_jumper : current_site.subtitle
-      end
-      
-      def site_jumper
-        render :partial => 'admin/shared/site_jumper'
-      end
       
     end
   end
